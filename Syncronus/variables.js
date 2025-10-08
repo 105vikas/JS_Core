4f6884143de15cf62f34f9b66902e8dbfd5237c1
vikasyadav <offivikas@gmail.com>
2025-10-08 22:19:09 +0530

    completed the variable type & scope

--- Syncronus/variables.js
+++ Syncronus/variables.js
@@ -1,3 +1,16 @@
 // ✅ Topic: Variables
-// Description: Add examples and notes related to Variables here.
 
+// let var const
+// let - Reassign (Block scope)
+// var - Ressignable & redefine (Global,functional scope)
+// const - constant value not (Block scope)
+
+// if there are define in there scope then they are not accessable out of that scope
+
+
+//Scope
+// Global 
+// function
+// Block
+
+// first search on it's own scope then out scope till the global scope don't come
