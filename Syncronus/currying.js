9212c0ba139bb0d7d84286449e3ad1881e85beec
vikasyadav <offivikas@gmail.com>
2025-10-13 00:20:55 +0530

    curring

--- Syncronus/currying.js
+++ Syncronus/currying.js
@@ -1,5 +1,30 @@
 // ✅ Topic: Currying
-// Description: Add examples and notes related to Currying here.
+
+function nation(nation) {
+    function state(state) {
+        function distict(distict) {
+            function village(village) {
+                return `Nation: ${nation},State: ${state}, District ${distict}, Village: ${village}`;
+            }
+            return village;
+        }
+        return distict;
+    }
+    return state;
+}
+
+let myNation = nation("India")
+let myState = myNation("Up")
+let mydistrict = myState("Prayagraj")
+let myVillage = mydistrict("Baraut")
+console.log(myVillage)
+let myFriVillage = mydistrict("Bhiti")
+console.log(myFriVillage)
+
+let friendState = myNation("Mp")
+let friendDistrict = friendState("GateWay")
+let friendVillage = friendDistrict("Village Mp")
+console.log(friendVillage)
 
 // factorial_
 // fibonacci
